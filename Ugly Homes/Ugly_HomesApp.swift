f0af96402b8424cbecf678583d6ea7c661ea4ddc
Patrick Arenson <patrickarenson@gmail.com>
2025-10-30 11:43:53 -0400

    Initial Commit

---
+++ Ugly Homes/Ugly_HomesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ugly_HomesApp.swift
+//  Ugly Homes
+//
+//  Created by Patrick Arenson on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Ugly_HomesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ugly HomesTests/Ugly_HomesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ugly_HomesTests.swift
+//  Ugly HomesTests
+//
+//  Created by Patrick Arenson on 10/30/25.
+//
+
+import Testing
+@testable import Ugly_Homes
+
+struct Ugly_HomesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
